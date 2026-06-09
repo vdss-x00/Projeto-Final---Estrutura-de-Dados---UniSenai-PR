5b15d6666bf016969fb8f44de6b89760933df25c
Nicolas-Oliveira543 <nicolasnsdeoliveira@gmail.com>
2026-06-09 19:05:04 -0300

    Remove sistemaLoja function and update menu options

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@ com os arquivos header (.hpp). Mas enfim, vamo desenvolver mais pra dps ver mais
 #include "./headers/buscas.hpp"
 #include "./headers/ordenacoes.hpp"
 
-void sistemaLoja(){
-//código vai aqui
-}
-
 void sistemaIngresso(){
 //código vai aqui
 }
@@ -20,19 +16,13 @@ int main(){
     int seletor;
 
     do{
-        std::cout << "Selecione o sistema que deseja acessar: " << std::endl;
-        std::cout << "1 - Sistema de Loja" << std::endl;
-        std::cout << "2 - Sistema de Ingresso" << std::endl;
+        std::cout << "1 - Sistema de Ingresso" << std::endl;
         std::cout << "0 - Sair" << std::endl;
         std::cin >> seletor;
 
         switch (seletor)
         {
         case 1:
-            sistemaLoja();
-            break;
-        
-        case 2:
             sistemaIngresso();
             break;
 
